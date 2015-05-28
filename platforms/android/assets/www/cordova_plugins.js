a4a0ec2438ae6672aae0cb4ebe550b9e10743b4f
dieos2 <dieos2@gmail.com>
2015-05-28 18:09:27 -0300

    Deu Jeito

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,18 +1,9 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/com.clone.phonegap.plugins.PushPlugin/www/PushNotification.js",
-        "id": "com.clone.phonegap.plugins.PushPlugin.PushNotification",
-        "clobbers": [
-            "PushNotification"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.geolocation": "0.3.7",
-    "com.clone.phonegap.plugins.PushPlugin": "2.4.1"
+    "org.apache.cordova.geolocation": "0.3.7"
 }
 // BOTTOM OF METADATA
 });
